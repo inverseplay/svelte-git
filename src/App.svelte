77f7fd54dbaad1d0c93e74d110cb5acab7b88965
inverseplay <inversedevelop@gmail.com>
2022-02-05 17:27:44 +0900

    fruit-c1

--- src/App.svelte
+++ src/App.svelte
@@ -1,10 +1,16 @@
 <script>
 import Footer from "./components/footer/footer.svelte";
+import Fruit from "./components/fruit/fruit.svelte";
 import Header from "./components/header/header.svelte";
 import Main from "./components/main/main.svelte";
 
+let fruits = ["사과","딸기","포도","배"]
 
 </script>
 <Header />
 <Main />
+<Fruit {fruits} name="일반 전개"  />
+<Fruit {fruits} reverse  name="역전개"/>
+<Fruit {fruits} slice="-2"  name="슬라이스 -2" />
+<Fruit {fruits} slice="0,4" reverse  name="슬라이스 0,4" />
 <Footer />
---
+++ src/components/fruit/fruit.svelte
@@ -0,0 +1,21 @@
+<script>
+
+export let fruits
+export let reverse
+export let slice
+export let name
+
+let computedFruits = []
+computedFruits = reverse ? [...fruits].reverse() : [...fruits]
+computedFruits = slice ? computedFruits.slice(...slice.split(',')) : computedFruits
+
+// 이러한 코드를 함수형으로 재정의 해보자
+
+</script>
+
+<h2>fruits {name} </h2>
+<ul>
+    {#each computedFruits as f }
+    <li>{f}</li>        
+    {/each}
+</ul>
