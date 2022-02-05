48d540188a7e6cf11eb01a7f4b9c1a4b024baec9
inverseplay <inversedevelop@gmail.com>
2022-02-05 18:07:17 +0900

    이것이 바로 철학

--- src/App.svelte
+++ src/App.svelte
@@ -13,7 +13,7 @@ let fn = f => [...f,"복숭아"]
 <Fruit {fruits} name="일반 전개"  />
 <Fruit {fruits} reverse  name="역전개"/>
 <Fruit {fruits} slice="-2"  name="슬라이스 -2" />
-<Fruit {fruits} slice="0,4" reverse  name="슬라이스 0,4" />
+<Fruit {fruits} slice="1,3"   name="슬라이스 1,3 = 포함~미포함" />
 <Fruit { fruits } {fn} name="복숭아 한개 추가"   />
 <Fruit fruits = { fn(fn(fruits))}  name="복숭아 두개 추가"  />
 <Footer />
