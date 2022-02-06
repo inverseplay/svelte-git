76e44272d42b1cb2cba73ac319d468fb6d2f93d4
inverseplay <inversedevelop@gmail.com>
2022-02-06 14:45:35 +0900

    setting-c1

---
+++ src/components/new_component/commands/content1.js
@@ -0,0 +1,6 @@
+`git log -p
+git log -stat
+git log --all --decorate --oneline --graph
+git log --decorate --oneline -g   
+git log --grep
+git log -S`
---
+++ src/components/new_component/new_component.svelte
@@ -0,0 +1,7 @@
+<script>
+import { data } from "./data/index";
+</script>
+
+<textarea name="" id="" cols="60" rows="10">
+    {data}
+</textarea>
